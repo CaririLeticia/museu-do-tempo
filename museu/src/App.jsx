9a23e07e3a6979493e6f17fdf75af718809407bd
pablo <stevaopabllo@gmail.com>
2025-07-07 09:43:35 -0300

    feat (Routes, Pages) Criado Routas e Page

--- museu/src/App.jsx
+++ museu/src/App.jsx
@@ -1,23 +1,9 @@
-import { useState } from 'react'
-import './App.css'
-import { Test } from './test'
-import { CadastroExposicao } from './components/CadastroExposicao'
-import { Footer } from './components/Footer'
-import { Header } from './components/Header'
-import { Carrousel } from './components/Carousel'
-import { TimeLine } from './components/TimeLine'
+import { Paths } from './routes/Paths'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      {/* <Test/>*/}
-      {/* <CadastroExposicao/> */}
-      {/* <Footer/> */}
-      {/* <Header/> */}
-      {/* <Carrousel/> */}
-      {/* <TimeLine/> */}
+        <Paths/>
     </>
   )
 }
---
+++ museu/src/Pages/Home.jsx
@@ -0,0 +1,11 @@
+import { Carrousel } from "../components/Carousel"
+import { TimeLine } from "../components/TimeLine"
+
+export const Home = () => {
+    return(
+        <>
+            <Carrousel/>
+            <TimeLine/>
+        </>
+    )
+}
