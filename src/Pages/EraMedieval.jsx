f1581fd92d0b1406b71e314094ef3ebb9bfa418b
Jorgemarley085 <132118506+Jorgemarley085@users.noreply.github.com>
2025-07-12 21:49:59 -0300

    alterando para teste

--- src/Pages/EraMedieval.jsx
+++ src/Pages/EraMedieval.jsx
@@ -44,7 +44,9 @@ export const EraMedieval = () => {
 
   return (
     <div className="p-4">
-      <h1 className="text-2xl font-bold mb-4">Obras da Era Medieval</h1>
+      <h1 className="text-2xl font-bold mb-4 text-center">
+        Obras da Era Medieval
+      </h1>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
         {obras.map((obra) => (
           <div
